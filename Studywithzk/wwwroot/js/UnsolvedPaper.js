6e92f87ef91cee32988bdedd2c3cb899a2bfd98d
zaeemkhan <khanzaeem010@gmail.com>
2022-07-18 12:51:44 +0500

    ckeditor configured with mathtype

--- Studywithzk/wwwroot/js/UnsolvedPaper.js
+++ Studywithzk/wwwroot/js/UnsolvedPaper.js
@@ -5,17 +5,63 @@
     GetStates(countryId, stateId);
     GetBoard(stateId, boardid);
 
-
-    ClassicEditor.create(document.querySelector('#editor'), {
-    })
-        //.plugins.addExternal('ckeditor_wiris', 'https://www.wiris.net/demo/plugins/ckeditor/', 'plugin.js')
-        //.config.toolbar_Full.push({ name: 'wiris', items: ['ckeditor_wiris_formulaEditor', 'ckeditor_wiris_formulaEditorChemistry'] })
-        //.then(editor => {
-        //	window.editor = editor;
-        //})
-        .catch(err => {
-            console.error(err.stack);
-        });
+    var mathElements = [
+        'math',
+        'maction',
+        'maligngroup',
+        'malignmark',
+        'menclose',
+        'merror',
+        'mfenced',
+        'mfrac',
+        'mglyph',
+        'mi',
+        'mlabeledtr',
+        'mlongdiv',
+        'mmultiscripts',
+        'mn',
+        'mo',
+        'mover',
+        'mpadded',
+        'mphantom',
+        'mroot',
+        'mrow',
+        'ms',
+        'mscarries',
+        'mscarry',
+        'msgroup',
+        'msline',
+        'mspace',
+        'msqrt',
+        'msrow',
+        'mstack',
+        'mstyle',
+        'msub',
+        'msup',
+        'msubsup',
+        'mtable',
+        'mtd',
+        'mtext',
+        'mtr',
+        'munder',
+        'munderover',
+        'semantics',
+        'annotation',
+        'annotation-xml'
+    ];
+    CKEDITOR.plugins.addExternal('ckeditor_wiris', 'https://ckeditor.com/docs/ckeditor4/4.19.0/examples/assets/plugins/ckeditor_wiris/', 'plugin.js');
+    CKEDITOR.replace('ContentData', {
+        extraPlugins: 'ckeditor_wiris',
+        // For now, MathType is incompatible with CKEditor file upload plugins.
+        removePlugins: 'uploadimage,uploadwidget,uploadfile,filetools,filebrowser',
+        height: 320,
+        // Update the ACF configuration with MathML syntax.
+        extraAllowedContent: mathElements.join(' ') + '(*)[*]{*};img[data-mathml,data-custom-editor,role](Wirisformula)',
+        removeButtons: 'PasteFromWord'
+    });
+    CKEDITOR.config.pasteFromWordPromptCleanup = false;
+    CKEDITOR.config.pasteFromWordRemoveFontStyles = false;
+    CKEDITOR.config.pasteFromWordRemoveStyles = false;
 });
 
 function GetStates(val, stateId) {
